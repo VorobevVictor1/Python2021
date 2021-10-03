2f1836860ec52c816f21fe991a77984427da4040
KorotnevVyacheslav <91067374+KorotnevVyacheslav@users.noreply.github.com>
2021-10-03 23:39:05 +0300

    Add files via upload

--- 2021.09.27/4.py
+++ 2021.09.27/4.py
@@ -10,7 +10,19 @@ WHITE=(255,255,255)
 screen = pygame.display.set_mode((1000, 1000))
 screen.fill((255,255,255))
 
-def pictureofbear(x,y,k):
+surf=pygame.Surface((1000,1000))
+surf.fill((255,255,255))
+
+def rot_center(image, angle):
+    """rotate an image while keeping its center and size"""
+    orig_rect = image.get_rect()
+    rot_image = pygame.transform.rotate(image, angle)
+    rot_rect = orig_rect.copy()
+    rot_rect.center = rot_image.get_rect().center
+    rot_image = rot_image.subsurface(rot_rect).copy()
+    return rot_image
+
+def pictureofbear(x,y,k,screen):
     circle(screen, WHITE, (x+k*100-k*10, y-k*10),3)
     circle(screen, WHITE, (x+k*100+k*40, y),3)
     circle(screen, WHITE, (x+k*100-k*40, y-k*10), 7)
@@ -33,27 +45,61 @@ def pictureofbear(x,y,k):
     ellipse(screen, (randint(0,255), randint(0,255), randint(0,255)), [x+k*320-k*50+k*10, y+k*100-k*20-k*100+k*200-k*20+k*10, k*100, k*40])
     lines(screen, BLACK, False, [[x+k*100, y+k*100], [x+k*120, y+k*100-k*20],[x+k*320, y+k*100-k*20-k*100],[x+k*320, y+k*100-k*20-k*100+k*200]], 5)
 
-def pictureoffish(x,y,k):
+def pictureoffish(x,y,k,screen):
     ellipse(screen, WHITE, [x, y+k*50, k*400, k*100])
     ellipse(screen, (0,0,0), [x, y+k*50, k*400, k*100], 2)
     polygon(screen, (0, 0, 0), [(x,y+k*100),(x-k*100,y+k*100-k*100),(x-k*100,y+k*100+k*100),(x,y+k*100)])
     circle(screen, (0, 0, 0), (x+k*300, y+k*100), k*20)
     circle(screen, (200, 0, 0), (x+k*300, y+k*100), k*10)
 
-rect(screen, (0, 255, 255), (0, 0, 1000, 300))
-rect(screen, (0, 0, 0), (0, 300, 1000, 10))
-circle(screen, (255, 255, 0), (700, 200), 100)
-circle(screen, (0, 255, 255), (700, 200), 80)
-rect(screen, (255, 255, 0), (600, 200, 200+5, 10))
-rect(screen, (255, 255, 0), (600+100, 200-100, 10, 200))
+"""for i in range(1,11,1):
+    angle=randint(0,360)
+    x=randint(0,1)
+    if x==1: xbool=True
+    if x==0: xbool=False
+    surf=rot_center(surf, angle)
+    surf=pygame.transform.flip(surf ,xbool,False)
+    pictureofbear(randint(0,800),randint(400,800),randint(1,20)/20,surf)
+    pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30,surf)
+    surf=pygame.transform.flip(surf ,xbool,False)
+    surf=rot_center(surf, -angle)"""
 
 for i in range(1,11,1):
-    pictureofbear(randint(0,800),randint(400,800),randint(1,20)/20)
-    pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30)
+    surf1=pygame.Surface((400,400))
+    surf1.fill((255,255,255))
+    surf2=pygame.Surface((300,300))
+    surf2.fill((255,255,255))
+    pygame.Surface.set_alpha(surf1,100)
+    pygame.Surface.set_alpha(surf2,100)
+    angle=randint(0,360)
+    x=randint(0,1)
+    if x==1: xbool=True
+    if x==0: xbool=False
+    surf1=rot_center(surf1, angle)
+    surf1=pygame.transform.flip(surf1 ,xbool,False)
+    surf1=rot_center(surf2, angle)
+    surf1=pygame.transform.flip(surf2 ,xbool,False)
+    pictureofbear(100,150,randint(1,20)/20,surf1)
+    pictureoffish(0,0,randint(1,20)/30,surf2)
+    surf.blit(surf1, (randint(0,800), randint(400,800)))
+    surf.blit(surf2, (randint(200,800), randint(400,800)))
 
 
+
+rect(surf, (0, 255, 255), (0, 0, 1000, 300))
+rect(surf, (0, 0, 0), (0, 300, 1000, 10))
+circle(surf, (255, 255, 0), (700, 200), 100)
+circle(surf, (0, 255, 255), (700, 200), 80)
+rect(surf, (255, 255, 0), (600, 200, 200+5, 10))
+rect(surf, (255, 255, 0), (600+100, 200-100, 10, 200))
+
+#surf=pygame.transform.rotate(surf,90)
+#sur=pygame.Surface(pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30))
 #pictureoffish(500,700,1)
 
+screen.blit(surf, (0, 0))
+
+#pygame.transform.flip(screen ,True ,False)
 
 pygame.display.update()
 clock = pygame.time.Clock()
