05051475d796ad5c32b588a1ffd2e6aaee0bf9a8
KorotnevVyacheslav <91067374+KorotnevVyacheslav@users.noreply.github.com>
2021-10-04 17:44:23 +0300

    Add files via upload
    Uluchsheni medvedi

--- 2021.09.27/4.py
+++ 2021.09.27/4.py
@@ -52,46 +52,49 @@ def pictureoffish(x,y,k,screen):
     circle(screen, (0, 0, 0), (x+k*300, y+k*100), k*20)
     circle(screen, (200, 0, 0), (x+k*300, y+k*100), k*10)
 
-"""for i in range(1,11,1):
-    angle=randint(0,360)
-    x=randint(0,1)
-    if x==1: xbool=True
-    if x==0: xbool=False
-    surf=rot_center(surf, angle)
-    surf=pygame.transform.flip(surf ,xbool,False)
-    pictureofbear(randint(0,800),randint(400,800),randint(1,20)/20,surf)
-    pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30,surf)
-    surf=pygame.transform.flip(surf ,xbool,False)
-    surf=rot_center(surf, -angle)"""
-
-for i in range(1,11,1):
-    surf1=pygame.Surface((400,400))
-    surf1.fill((255,255,255))
-    surf2=pygame.Surface((300,300))
-    surf2.fill((255,255,255))
-    pygame.Surface.set_alpha(surf1,100)
-    pygame.Surface.set_alpha(surf2,100)
-    angle=randint(0,360)
-    x=randint(0,1)
-    if x==1: xbool=True
-    if x==0: xbool=False
-    surf1=rot_center(surf1, angle)
-    surf1=pygame.transform.flip(surf1 ,xbool,False)
-    surf1=rot_center(surf2, angle)
-    surf1=pygame.transform.flip(surf2 ,xbool,False)
-    pictureofbear(100,150,randint(1,20)/20,surf1)
-    pictureoffish(0,0,randint(1,20)/30,surf2)
-    surf.blit(surf1, (randint(0,800), randint(400,800)))
-    surf.blit(surf2, (randint(200,800), randint(400,800)))
-
-
-
 rect(surf, (0, 255, 255), (0, 0, 1000, 300))
 rect(surf, (0, 0, 0), (0, 300, 1000, 10))
 circle(surf, (255, 255, 0), (700, 200), 100)
 circle(surf, (0, 255, 255), (700, 200), 80)
 rect(surf, (255, 255, 0), (600, 200, 200+5, 10))
 rect(surf, (255, 255, 0), (600+100, 200-100, 10, 200))
+
+
+for i in range(1,11,1):
+    x=randint(0,1)
+    if x==1: xbool=True
+    if x==0: xbool=False
+    surf=pygame.transform.flip(surf ,xbool,False)
+    pictureofbear(randint(0,800),randint(400,800),randint(1,20)/20,surf)
+    pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30,surf)
+    surf=pygame.transform.flip(surf ,xbool,False)
+
+'''
+for i in range(5):
+    surf1=pygame.Surface((400,400))
+    surf1.fill((255,255,255))
+    surf2=pygame.Surface((300,300))
+    surf2.fill((255,255,255))
+    #pygame.Surface.set_alpha(surf1,100)
+    #pygame.Surface.set_alpha(surf2,100)
+
+    x=randint(0,1)
+    if x==1: xbool=True
+    if x==0: xbool=False
+
+    pictureofbear(100,150,randint(1,20)/20,surf1)
+    pictureofbear(100,100,1,surf1)
+    #pictureoffish(0,0,randint(1,20)/30,surf2)
+    surf1=pygame.transform.flip(surf1 , xbool ,False)
+    surf1=rot_center(surf2, angle)
+    surf1=pygame.transform.flip(surf2 ,xbool,False)
+
+    surf.blit(surf1, (randint(0,800), randint(400,800)))
+    surf.blit(surf1, (400, 200))
+    surf.blit(surf2, (randint(200,800), randint(400,800)))
+'''
+
+
 
 #surf=pygame.transform.rotate(surf,90)
 #sur=pygame.Surface(pictureoffish(randint(200,800),randint(400,800),randint(1,20)/30))
